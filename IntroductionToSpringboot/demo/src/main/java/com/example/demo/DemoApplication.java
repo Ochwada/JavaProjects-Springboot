d6891e9d1153fdfe080c90c53200d8d7c05e4d49
ochwada <nakhulolinda@yahoo.com>
2025-06-20 11:10:52 +0200

    initial project setup

--- IntroductionToSpringboot/demo/src/main/java/com/example/demo/DemoApplication.java
+++ IntroductionToSpringboot/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringApplication: is a helper class to launch our Spring Boot app
+ * @SpringBootApplication -> is a meta annotation that combines:
+ *  - @Configuration : makes the class as a source of spring beans.
+ *  - @EnableAutoConfiguration : enables automatic configuration based on classpath.
+ *  - @ComponentScan : scans the packages for Spring Components e.g. @RestController, @Service e.t.c.
+ */
+
 @SpringBootApplication
 public class DemoApplication {
 
